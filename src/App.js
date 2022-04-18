f94fb826be1cd036498fd74ecbf0e9490a4ada05
mohitraj610 <mohitraj1999@gmail.com>
2022-04-18 13:47:32 +0530

    V6

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import Home from "./Components/Pages/Home";
 import Leaderboard from "./Components/Pages/Leaderboard";
 import Navbar from "./Components/Layout/Navbar";
 import Game from "./Components/Pages/Game"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
       <div className="cc">
         <div className="navstick">
         <Navbar />
         </div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/leaderboard" component={Leaderboard} />
-          <Route path="/game" component={Game} />
-        </Switch>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/leaderboard" exact element={<Leaderboard />} />
+          <Route path="/game" exact element={<Game />} />
+        </Routes>
       </div>
-    </Router>
   );
 }
 
